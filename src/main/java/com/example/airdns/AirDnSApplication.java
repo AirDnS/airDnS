7259ec39d95f625b6dbb29cfcb7d729265e305f5
Kim-Jong-Gyu <whdrb2643@gmail.com>
2024-01-05 12:17:08 +0900

    first init

---
+++ src/main/java/com/example/airdns/AirDnSApplication.java
@@ -0,0 +1,13 @@
+package com.example.airdns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirDnSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirDnSApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/airdns/AirDnSApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airdns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirDnSApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
