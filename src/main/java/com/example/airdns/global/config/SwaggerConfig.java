b148ddc6c4db2c7a59b8abaae83cc40ff0b7824a
Kim-Jong-Gyu <whdrb2643@gmail.com>
2024-01-11 10:32:50 +0900

    Fix : "어노테이션명 수정"
    - SwaggerUI 설정 어노테이션 명 수정
    
    Resolves: #13

--- src/main/java/com/example/airdns/global/config/SwaggerConfig.java
+++ src/main/java/com/example/airdns/global/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.example.airdns.global.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.context.annotation.Bean;
@@ -12,7 +13,7 @@ import org.springframework.context.annotation.Configuration;
         description = "AirDns App Api 명세서",
         version = "v1"))
 
-@io.swagger.v3.oas.annotations.security.SecurityScheme(name = "Bearer Authentication",
+@SecurityScheme(name = "Bearer Authentication",
         type = SecuritySchemeType.HTTP,
         bearerFormat = "JWT",
         scheme = "Bearer")
