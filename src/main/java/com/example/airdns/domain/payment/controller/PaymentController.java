769ffc1d5cf4c01360d8ba1dbb4bbcfcd39aa41c
rinyejo <rinyejoin@gmail.com>
2024-01-16 10:18:50 +0900

    resolved #39

--- src/main/java/com/example/airdns/domain/payment/controller/PaymentController.java
+++ src/main/java/com/example/airdns/domain/payment/controller/PaymentController.java
@@ -1,32 +1,23 @@
 package com.example.airdns.domain.payment.controller;
 
 
-import com.example.airdns.domain.payment.dto.PaymentReqDto;
-import com.example.airdns.domain.payment.dto.PaymentResDto;
-import com.example.airdns.domain.payment.service.TossPaymentServiceImplV1;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/payment")
+@RequiredArgsConstructor
 public class PaymentController {
-
-    private final TossPaymentServiceImplV1 tossPaymentService;
-
-    @Autowired
-    public PaymentController(TossPaymentServiceImplV1 tossPaymentService) {
-        this.tossPaymentService = tossPaymentService;
-    }
-
-    @PostMapping("/request")
-    public ResponseEntity<?> processPayment(@RequestBody PaymentReqDto paymentReqDto) {
-        PaymentResDto responseDto = tossPaymentService.processPayment(paymentReqDto)
-                .toPaymentResDto();
-        return ResponseEntity.ok(responseDto);
-    }
+//
+//    private final TossPaymentServiceImplV1 tossPaymentService;
+//
+//
+//    @PostMapping("/request")
+//    public ResponseEntity<?> processPayment(@RequestBody PaymentReqDto paymentReqDto) {
+//        PaymentResDto responseDto = tossPaymentService.processPayment(paymentReqDto)
+//                .toPaymentResDto();
+//        return ResponseEntity.ok(responseDto);
+//    }
 
 }
