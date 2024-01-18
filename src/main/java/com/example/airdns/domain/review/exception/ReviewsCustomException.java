023783db7f3ddb8b58ce436c359eea15cc9c4f53
parkjihwan <mankcom7708@gmail.com>
2024-01-18 23:20:16 +0900

    Fix : "[Reviews] Exception 처리가 코드 컨벤션에서 어긋남"
    Reviews 관련된 Exception 삭제
    
    fixed : #74

--- src/main/java/com/example/airdns/domain/review/exception/ReviewsCustomException.java
+++ src/main/java/com/example/airdns/domain/review/exception/ReviewsCustomException.java
@@ -3,7 +3,6 @@ package com.example.airdns.domain.review.exception;
 import com.example.airdns.global.exception.CustomException;
 
 public class ReviewsCustomException extends CustomException {
-
     public ReviewsCustomException(ReviewsExceptionCode exceptionCode){
         super(exceptionCode.getHttpStatus(), exceptionCode.getErrorCode(), exceptionCode.getMessage());
     }
