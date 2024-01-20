480d301e16b2a5bad80058d0beb8ee3f579d7a12
Kim-Jong-Gyu <whdrb2643@gmail.com>
2024-01-20 20:56:11 +0900

    Fix: "WebSecurity Cors 경로 수정"
    - 프론트 배포로 인한 경로 수정
    
    Resolves: #89

--- src/main/java/com/example/airdns/global/config/WebSecurityConfig.java
+++ src/main/java/com/example/airdns/global/config/WebSecurityConfig.java
@@ -42,7 +42,7 @@ public class WebSecurityConfig {
         return request -> {
             CorsConfiguration config = new CorsConfiguration();
             config.setAllowCredentials(true);
-            config.setAllowedOrigins(List.of("http://localhost:3000"));
+            config.setAllowedOrigins(List.of("https://d3sjw1jlqtqto3.cloudfront.net"));
             config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
             config.setAllowedHeaders(List.of("*"));
             config.setExposedHeaders(List.of("*"));
