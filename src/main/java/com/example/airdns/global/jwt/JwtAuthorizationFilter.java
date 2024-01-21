89c294543245e33c1bb09bf12603365a6f2baf68
Kim-Jong-Gyu <whdrb2643@gmail.com>
2024-01-21 19:59:25 +0900

    Test: "CD 구동 테스트"
    Resolves: #91

--- src/main/java/com/example/airdns/global/jwt/JwtAuthorizationFilter.java
+++ src/main/java/com/example/airdns/global/jwt/JwtAuthorizationFilter.java
@@ -27,6 +27,7 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+        System.out.println("test");
         String token = request.getHeader(AUTHORIZATION_HEADER);
 
         if (StringUtils.hasText(token)) {
