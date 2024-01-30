b27792ec817fbfab743e1a85d52a2060480105e8
parkjihwan <mankcom7708@gmail.com>
2024-01-30 14:57:38 +0900

    Refactor : [Deletion_Management] Scheduler 리팩토링
    Scheduler 설정 변경
    
    Fixed : #152

--- src/main/java/com/example/airdns/global/scheduler/Scheduler.java
+++ src/main/java/com/example/airdns/global/scheduler/Scheduler.java
@@ -22,11 +22,11 @@ public class Scheduler {
 
     // 초 분 시 일 월 요일
     //@Transactional
-    //@Scheduled(cron = "0 0 1 * * *")
-    @Scheduled(cron = "10 * * * * *")
+    @Scheduled(cron = "0 0 1 * * *")
+    //@Scheduled(cron = "10 * * * * *")
     public void deleteEntities(){
-        LocalDateTime deleteTime = LocalDateTime.now().minusDays(1);
-        //LocalDateTime deleteTime = LocalDateTime.now().minusYears(1);
+        //LocalDateTime deleteTime = LocalDateTime.now().minusDays(1);
+        LocalDateTime deleteTime = LocalDateTime.now().minusYears(1);
 
         // deleted_at으로부터 1년이 지난 entity의 인스턴스들을 삭제
         usersService.deleteUsers(deleteTime);
