7947464a662d7a4a8d3c2f39597626f7f49dc4bb
parkjihwan <mankcom7708@gmail.com>
2024-01-31 22:12:27 +0900

    Test : [Like] 전체 좋아요 조회에서 방에 대한 좋아요 변경으로 인한 테스트 코드 수정
    LikesRepository method 일부 삭제 및
    LikesRepositoryTest 테스트 코드 수정
    
    fixed : #131

--- src/test/java/com/example/airdns/like/LikesRepositoryTest.java
+++ src/test/java/com/example/airdns/like/LikesRepositoryTest.java
@@ -1,119 +1,72 @@
 package com.example.airdns.like;
 
 import com.example.airdns.domain.like.entity.Likes;
-import com.example.airdns.domain.like.repository.LikesRepository;
 import com.example.airdns.domain.room.entity.Rooms;
 import com.example.airdns.domain.user.entity.Users;
+import com.example.airdns.domain.like.repository.LikesRepository;
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
-@DataJpaTest
+@SpringBootTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class LikesRepositoryTest {
 
     @Autowired
-    private LikesRepository likesRepository;
+    LikesRepository likesRepository;
 
     @Autowired
-    private TestEntityManager entityManager;
-
-    @Test
-    @DisplayName("LikesRepository deleteByRoomsId Success")
-    void deleteByRoomsIdSuccess() {
-        // given
-        Rooms room1 = Rooms.builder().name("Room Number1").build();
-        entityManager.persist(room1);
-
-        Likes like = Likes.builder().rooms(room1).build();
-        entityManager.persist(like);
-        entityManager.flush();
-
-        // when
-        likesRepository.deleteByRoomsId(like.getRooms().getId());
-
-        // then
-        List<Likes> likesList = likesRepository.findAll();
-        assertEquals(0, likesList.size());
-    }
-
-    @Test
-    @DisplayName("LikesRepository findAllByRoomsId Success")
-    void findAllByRoomsIdSuccess() {
-        // given
-        Rooms room1 = Rooms.builder().name("room number1").build();
-        Rooms room2 = Rooms.builder().name("room number2").build();
-
-        entityManager.persist(room1);
-        entityManager.persist(room2);
-        entityManager.flush();
-
-        Likes like1 = Likes.builder().rooms(room1).build();
-        Likes like2 = Likes.builder().rooms(room1).build();
-        Likes like3 = Likes.builder().rooms(room2).build();
-
-        entityManager.persist(like1);
-        entityManager.persist(like2);
-        entityManager.persist(like3);
-        entityManager.flush();
-
-        // when
-        List<Likes> likesList = likesRepository.findAllByRoomsId(room1.getId());
-
-        // then
-        assertEquals(2, likesList.size());
-    }
+    EntityManager entityManager;
 
     @Test
     @Transactional
     @DisplayName("LikesRepository findByRoomsId Success")
-    void findByRoomsIdSuccess() {
-        // given
-        Rooms room1 = Rooms.builder().name("room number1").build();
+    public void findByRoomsIdSuccess() {
+        // Likes 엔티티를 생성하고 저장
+        Users user = Users.builder().nickname("User Nickname").build();
+        Rooms room = Rooms.builder().users(user).name("Room Name").build();
 
-        entityManager.persist(room1);
-        entityManager.flush();
+        entityManager.persist(room);
+        entityManager.persist(user);
 
-        Likes like = Likes.builder().rooms(room1).build();
-        entityManager.persist(like);
-        entityManager.flush();
+        Likes likes = Likes.builder().users(user).rooms(room).build();
+        entityManager.persist(likes);
 
-        // when
-        Optional<Likes> foundLike = likesRepository.findByRoomsId(1L);
+        Optional<Likes> foundLikes = likesRepository.findByRoomsId(room.getId());
 
-        // then
-        // 이거 테스트 머리 식히고 하겠음...
-        // assertEquals(like, foundLike.orElse(null));
+        assertTrue(foundLikes.isPresent());
+
+        assertEquals(room.getId(), foundLikes.get().getRooms().getId());
     }
 
     @Test
-    @DisplayName("LikesRepository findBuRoomsIdAndUsersId Success")
-    void findByRoomsIdAndUsersIdSuccess() {
-        // given
-        Rooms room1 = Rooms.builder().name("room number1").build();
-        // Users user1 = Users.builder().nickName("users number1").build();
+    @Transactional
+    @DisplayName("LikesRepository ExistsByRoomsAndUsers Success")
+    public void ExistsByRoomsAndUsers() {
+        // Likes 엔티티를 생성하고 저장
+        Users user = Users.builder().nickname("User Nickname").build();
+        Rooms room = Rooms.builder().users(user).name("Room Name").build();
 
-        entityManager.persist(room1);
-        // entityManager.persist(user1);
-        entityManager.flush();
+        entityManager.persist(room);
+        entityManager.persist(user);
 
-        // Likes like = Likes.builder().rooms(room1).users(user1).build();
-        // entityManager.persist(like);
-        entityManager.flush();
+        Likes likes = Likes.builder().users(user).rooms(room).build();
+        entityManager.persist(likes);
 
-        // when
-        // Optional<Likes> foundLike = likesRepository.findByRoomsIdAndUsersId(room1.getId(), user1.getId());
+        // LikesRepository를 사용하여 existsByRoomsAndUsers 메서드를 호출
+        boolean exists = likesRepository.existsByRoomsAndUsers(room, user);
 
-        // then
-        // assertEquals(like, foundLike.orElse(null));
+        // 해당 Rooms와 Users로 좋아요가 존재해야 합니다.
+        assertTrue(exists);
     }
 }
