af7eea1f537a857f72f07248bd1b044e4cb21344
rinyejo <rinyejoin@gmail.com>
2024-02-06 15:00:09 +0900

    Refactor: "누락된 swagger 적용" - Request Dto, Response Dto 모두 적용 Resolves #214

--- src/main/java/com/example/airdns/domain/payment/dto/PaymentRequestDto.java
+++ src/main/java/com/example/airdns/domain/payment/dto/PaymentRequestDto.java
@@ -1,5 +1,6 @@
 package com.example.airdns.domain.payment.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.io.Serializable;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,14 +13,27 @@ public class PaymentRequestDto {
     @Builder
     @NoArgsConstructor
     @AllArgsConstructor
+    @Schema(description = "Create Payment request")
     public static class CreatePaymentRequestDto implements Serializable {
-
+        @Schema(description = "결제 수단", example = "CARD")
         private String paymentType;
+
+        @Schema(description = "결제 고유 값", example = "1qwerksldlfkalskdf")
         private String paymentKey;
+
+        @Schema(description = "결제 주문 id", example = "sdfqwefaasd1as")
         private String orderId;
+
+        @Schema(description = "결제 id", example = "1")
         private Long id;
+
+        @Schema(description = "결제 금액", example = "10000")
         private Long amount;
+
+        @Schema(description = "결제 이름", example = "아름다운 방")
         private String orderName;
+
+        @Schema(description = "예약 번호", example = "1")
         private Long reservationId;
     }
 }
--- src/main/java/com/example/airdns/domain/payment/dto/PaymentResponseDto.java
+++ src/main/java/com/example/airdns/domain/payment/dto/PaymentResponseDto.java
@@ -1,6 +1,7 @@
 package com.example.airdns.domain.payment.dto;
 
 import com.example.airdns.domain.payment.entity.Payment;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.io.Serializable;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,12 +15,22 @@ public class PaymentResponseDto {
     @Builder
     @NoArgsConstructor
     @AllArgsConstructor
+    @Schema(description = "Create Payment request")
     public static class CreatePaymentResponseDto implements Serializable {
 
+        @Schema(description = "결제 id", example = "1")
         private Long id;
+
+        @Schema(description = "결제 수단", example = "CARD")
         private String paymentType;
+
+        @Schema(description = "결제 금액", example = "10000")
         private Long amount;
+
+        @Schema(description = "결제 이름", example = "아름다운 방")
         private String orderName;
+
+        @Schema(description = "예약 번호", example = "1")
         private Long reservationId;
 
         public static CreatePaymentResponseDto from(Payment payment) {
@@ -38,12 +49,23 @@ public class PaymentResponseDto {
     @Builder
     @NoArgsConstructor
     @AllArgsConstructor
+    @Schema(description = "Read Payment")
     public static class ReadPaymentResponseDto implements Serializable {
 
+
+        @Schema(description = "결제 id", example = "1")
         private Long id;
+
+        @Schema(description = "결제 수단", example = "CARD")
         private String paymentType;
+
+        @Schema(description = "결제 금액", example = "10000")
         private Long amount;
+
+        @Schema(description = "결제 이름", example = "아름다운 방")
         private String orderName;
+
+        @Schema(description = "예약 번호", example = "1")
         private Long reservationId;
 
         public static ReadPaymentResponseDto from(Payment payment) {
