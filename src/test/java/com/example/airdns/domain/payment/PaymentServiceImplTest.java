d48c80747e548c9f210498706cd3c0623b29b525
rinyejo <rinyejoin@gmail.com>
2024-02-08 00:41:37 +0900

    Test: "Payment Service 테스트 코드 작성" Resolves #226

--- src/test/java/com/example/airdns/domain/payment/PaymentServiceImplTest.java
+++ src/test/java/com/example/airdns/domain/payment/PaymentServiceImplTest.java
@@ -2,14 +2,18 @@ package com.example.airdns.domain.payment;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import com.example.airdns.domain.payment.dto.PaymentResponseDto;
 import com.example.airdns.domain.payment.entity.Payment;
+import com.example.airdns.domain.payment.exception.PaymentCustomException;
+import com.example.airdns.domain.payment.exception.PaymentExceptionCode;
 import com.example.airdns.domain.payment.repository.PaymentRepository;
 import com.example.airdns.domain.payment.service.PaymentServiceImplV1;
 import com.example.airdns.domain.reservation.entity.Reservation;
+import com.example.airdns.domain.reservation.repository.ReservationRepository;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,12 +27,14 @@ public class PaymentServiceImplTest {
 
     @Mock
     private PaymentRepository paymentRepository;
+    @Mock
+    private ReservationRepository reservationRepository;
 
     @InjectMocks
     private PaymentServiceImplV1 paymentService;
 
     @Test
-    @DisplayName("PaymentService readPayment Success")
+    @DisplayName("결제 조회 성공 케이스")
     void readPaymentSuccess() {
         // given
         Long reservationId = 1L;
@@ -65,4 +71,25 @@ public class PaymentServiceImplTest {
         verify(paymentRepository, times(1))
                 .findByReservationIdAndIdAndIsDeletedFalse(reservationId, paymentId);
     }
+
+    @Test
+    @DisplayName("결제 조회 실패 케이스 - 결제 내역이 없는 경우")
+    void readPaymentFailWhenPaymentNotExist() {
+        // given
+        Long reservationId = 1L;
+        Long paymentId = 2L;
+        when(paymentRepository.findByReservationIdAndIdAndIsDeletedFalse(reservationId, paymentId))
+                .thenThrow(new PaymentCustomException(PaymentExceptionCode.NOT_FOUND_MATCHED_PAYMENT));
+
+        // when & then
+        Exception exception = assertThrows(
+                PaymentCustomException.class,
+                () -> paymentService.readPayment(reservationId, paymentId),
+                "예약번호와 일치하는 결제 내역이 존재하지 않습니다."
+        );
+
+        verify(paymentRepository, times(1))
+                .findByReservationIdAndIdAndIsDeletedFalse(reservationId, paymentId);
+    }
+
 }
